64e5901a8fa312b14630ff6dc10cc42364b9b7c8
Harddik Bafna <106113538+Zardian18@users.noreply.github.com>
2024-01-01 13:25:58 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -44,7 +44,38 @@ import json
 import os
 import pickle
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
+
+def sample_batch(dataset):
+    batch = dataset.take(1).get_single_element()
+    if isinstance(batch, tuple):
+        batch = batch[0]
+    return batch.numpy()
+
+
+def display(
+    images, n=10, size=(20, 3), cmap="gray_r", as_type="float32", save_to=None
+):
+    """
+    Displays n random images from each one of the supplied arrays.
+    """
+    if images.max() > 1.0:
+        images = images / 255.0
+    elif images.min() < 0.0:
+        images = (images + 1.0) / 2.0
+
+    plt.figure(figsize=size)
+    for i in range(n):
+        _ = plt.subplot(1, n, i + 1)
+        plt.imshow(images[i].astype(as_type), cmap=cmap)
+        plt.axis("off")
+
+    if save_to:
+        plt.savefig(save_to)
+        print(f"\nSaved to {save_to}")
+
+    plt.show()
 # Create a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(filename, img_shape=224, scale=True):
   """
